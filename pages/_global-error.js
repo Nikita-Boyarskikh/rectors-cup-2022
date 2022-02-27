7fc427ad6a785a6fd900d9c27b0ad889b7b359c1
Nikita Boyarskikh <n.boyarskikh@getdisco.com>
2022-02-27 18:08:44 +0300

    Add google table link to error page

--- pages/_global-error.js
+++ pages/_global-error.js
@@ -1,10 +1,16 @@
+import config from '../config';
+
 const GlobalError = () => {
+  const googleTableLink = `https://docs.google.com/spreadsheets/d/${config.api.spreadsheetId}`
+
   return (
     <main>
       <h1>Упс! Что-то пошло не так :(</h1>
       <div>
         Сообщите об ошибке нам по <a href="mailto:nboyarskikh@gmail.com">email</a>,
         и мы постараемся поправить как можно скорее
+
+        А пока можете посмотреть результаты в гугл-таблице: <a href={googleTableLink}>{googleTableLink}</a>
       </div>
     </main>
   )
