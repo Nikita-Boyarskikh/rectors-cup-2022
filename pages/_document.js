85bcbd810086b5664abeab7b8ab46281de678eb1
Nikita Boyarskikh <n.boyarskikh@getdisco.com>
2022-03-05 14:22:17 +0300

    Finish optimization 100 lighthouse points

--- pages/_document.js
+++ pages/_document.js
@@ -2,14 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 import Favicons from 'components/favicons'
 
-export default function Document() {
+const Document = () => {
   return (
     <Html lang="ru" dir="ltr">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" key="googleapisPreconnect" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" key="gstaticPreconnect" />
-        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet" key="russo" />
-        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" key="rubik" />
+        <link href="https://fonts.googleapis.com/css2?family=Russo+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet" />
         <Favicons />
       </Head>
       <body>
@@ -19,3 +17,5 @@ export default function Document() {
     </Html>
   )
 }
+
+export default Document
